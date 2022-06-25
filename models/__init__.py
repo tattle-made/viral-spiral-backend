6c1e49a9cc3718bd42c09870c304b38078a458b3
Rishav Thakker <thakker.rishav@gmail.com>
2022-06-25 17:56:13 +0530

    Added setup file

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,20 @@
 """Easy imports for models"""
-from .base import Game
+from .base import Game, db
 from .card import Card, CardInstance
 from .player import Player, PlayerInitialBias, PlayerInitialAffinity
 from .counters import AffinityTopic, Color
 from .powers import PlayerPower
 from .card_queue import PlayerCardQueue
+
+ALL_MODELS = [
+    Game,
+    Card,
+    CardInstance,
+    Player,
+    PlayerInitialBias,
+    PlayerInitialAffinity,
+    AffinityTopic,
+    Color,
+    PlayerPower,
+    PlayerCardQueue,
+]
