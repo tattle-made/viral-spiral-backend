0581a5256bb02851e13ae467ab2d473124f96e9c
Rishav Thakker <thakker.rishav@gmail.com>
2022-11-11 13:49:27 +0530

    created activate_env.sh
    To separate env activation from startup script

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
 echo "Starting viral spiral backend"
-export PYTHONPATH=$(pwd):$PYTHONPATH
-[ -z "$DB_NAME" ] && export DB_NAME="tattleviralspiral"
-[ -z "$DB_HOST" ] && export DB_HOST="127.0.0.1"
-[ -z "$DB_PORT" ] && export DB_PORT=3306
-[ -z "$DB_USERNAME" ] && export DB_USERNAME="root"
-[ -z "$DB_PASSWORD" ] && export DB_PASSWORD="helloworld"
+source activate_env.sh
 pipenv run python main_loop/websocket.py
