7b8293038dd5648d7284338cc686463c443b3ad6
Rishav Thakker <thakker.rishav@gmail.com>
2022-11-16 23:20:51 +0530

    Fix - gevent

--- main_loop/websocket.py
+++ main_loop/websocket.py
@@ -33,7 +33,7 @@ root_logger = logging.getLogger("root")
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
-async_mode = None
+async_mode = "gevent"
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET")
