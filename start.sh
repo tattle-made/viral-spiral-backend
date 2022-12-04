69df98523a0b03b4b0db5fad9951b994a6b1883f
Rishav Thakker <thakker.rishav@gmail.com>
2022-12-04 12:43:26 +0530

    Added (commented) option for running app
    using eventlet and gunicorn

--- start.sh
+++ start.sh
@@ -3,3 +3,4 @@ source activate_env.sh
 pipenv run python main_loop/websocket.py
 # pipenv run gunicorn -b 0.0.0.0:5000 -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 --threads 10 main_loop.websocket:app
 # pipenv run gunicorn -b 0.0.0.0:5000 -w 1 --threads 100 main_loop.websocket:app
+# pipenv run gunicorn -b 0.0.0.0:5000 --worker-class eventlet --threads 100 main_loop.websocket:app
