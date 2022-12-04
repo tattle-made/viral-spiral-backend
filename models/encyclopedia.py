0a4b012db255624316414792fd67c5caafd36a5f
Rishav Thakker <thakker.rishav@gmail.com>
2022-12-04 12:52:31 +0530

    Added headline to encyclopedia

--- models/encyclopedia.py
+++ models/encyclopedia.py
@@ -7,4 +7,5 @@ class Article(InGameModel):
     # TODO Use a list of keywords instead of text search
     content_lower = peewee.TextField()  # For easier querying
     content = peewee.TextField()
+    headline = peewee.TextField()
     is_fake = peewee.BooleanField()
