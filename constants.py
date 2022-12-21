ec90d5a785b7c447df574e2a423e505eb9835b28
Rishav Thakker <i@rishavthakker.com>
2022-12-21 17:33:47 +0530

    Enabled all special powers

--- constants.py
+++ constants.py
@@ -2,15 +2,15 @@
 
 # If the affinity towards any topic reaches this count, the player gains the
 # viral spiral power
-VIRAL_SPIRAL_AFFINITY_COUNT = 7
-VIRAL_SPIRAL_BIAS_COUNT = 7
+VIRAL_SPIRAL_AFFINITY_COUNT = 0
+VIRAL_SPIRAL_BIAS_COUNT = 0
 
 # If the affinity towards any topic reaches 3, the player receives the
 # cancel power
-CANCELLING_AFFINITY_COUNT = 3
+CANCELLING_AFFINITY_COUNT = 0
 
 # If bias against any topic reaches 3, the player receives the fake news power
-FAKE_NEWS_BIAS_COUNT = 3
+FAKE_NEWS_BIAS_COUNT = 0
 
 ACTIVE_STR = "active"
 
