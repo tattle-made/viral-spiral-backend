e248eaebc195b390a3944b65af43a2fa9b79cf14
Denny <denny.george90@gmail.com>
2023-03-17 16:47:45 +0530

    feat: integrate new score with the rest of the game
    I don't like how Player and Score are now in one large file. Have created #83 to fix this later.

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,11 @@
 """Easy imports for models"""
 from .base import Game, Round, FullRound, db
 from .card import Card, CardInstance
-from .player import Player, PlayerInitialBias, PlayerInitialAffinity
+from .player import Player, PlayerInitialBias, PlayerInitialAffinity, Score
 from .counters import AffinityTopic, Color
 from .powers import PlayerPower, CancelStatus, CancelVote
 from .card_queue import PlayerCardQueue
 from .encyclopedia import Article
-from .score import Score
 
 ALL_MODELS = [
     # This is the order in which the tables will be created
