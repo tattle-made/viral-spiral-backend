95a197dccf8ecf15d5cc4829aa682256b36cd155
Denny <denny.george90@gmail.com>
2023-03-23 12:15:03 +0530

    chore

--- constants.py
+++ constants.py
@@ -20,7 +20,7 @@ FAKE_NEWS_BIAS_COUNT = 0
 ACTIVE_STR = "active"
 
 # If a player reaches a number of points, they win and the game ends
-PLAYER_WIN_SCORE = 50000
+PLAYER_WIN_SCORE = 15
 # If the TBG reaches a number of points, the game ends
 TGB_END_SCORE = 15
 
