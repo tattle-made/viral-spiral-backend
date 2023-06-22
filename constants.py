5eec5778848eb99c924189d985c8ac1087c27933
Bhargav-Dave <bhargavdave2404@gmail.com>
2023-06-22 09:57:44 +0530

    fix: updated constants

--- constants.py
+++ constants.py
@@ -10,21 +10,21 @@ VIRAL_SPIRAL_BIAS_COUNT = 5
 
 # If the affinity towards any topic reaches 3, the player receives the
 # cancel power
-CANCELLING_AFFINITY_COUNT = 1
+CANCELLING_AFFINITY_COUNT = 2
 # if true, cancel has the polling mechanic
 CANCELLING_ALLOW_POLL = False
 # If True, all players will be asked to vote instead of just the affinity
 CANCEL_VOTE_ALL_PLAYERS = False
 
 # If bias against any topic reaches 3, the player receives the fake news power
-FAKE_NEWS_BIAS_COUNT = 0
+FAKE_NEWS_BIAS_COUNT = 2
 
 ACTIVE_STR = "active"
 
 # If a player reaches a number of points, they win and the game ends
 PLAYER_WIN_SCORE = 7  # changed for a quicker game
 # If the TBG reaches a number of points, the game ends
-TGB_END_SCORE = 7
+TGB_END_SCORE = 15
 
 SOCKET_EVENT_ENC_SEARCH_RESULT = "encyclopedia_search_result"
 
