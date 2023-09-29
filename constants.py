1f4d8feb9130252146a489a40abcb40b5e0ee143
Denny <denny.george90@gmail.com>
2023-09-29 11:04:41 +0530

    chore: change constants

--- constants.py
+++ constants.py
@@ -5,12 +5,12 @@ NUM_AFFINITY_TOPICS = 2
 
 # If the affinity towards any topic reaches this count, the player gains the
 # viral spiral power
-VIRAL_SPIRAL_AFFINITY_COUNT = 4
-VIRAL_SPIRAL_BIAS_COUNT = 4
+VIRAL_SPIRAL_AFFINITY_COUNT = 0
+VIRAL_SPIRAL_BIAS_COUNT = 0
 
 # If the affinity towards any topic reaches 3, the player receives the
 # cancel power
-CANCELLING_AFFINITY_COUNT = 2
+CANCELLING_AFFINITY_COUNT = 0
 # if true, cancel has the polling mechanic
 CANCELLING_ALLOW_POLL = False
 # If True, all players will be asked to vote instead of just the affinity
